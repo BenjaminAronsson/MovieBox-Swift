38191ad6b4a8fc4f06e2c111f292ada023ecfb0d
dev <bene.a99@gmail.com>
2019-03-12 19:41:42 +0100

    Initial Commit

---
+++ MovieBox-Swift/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MovieBox-Swift
+//
+//  Created by Benjamin on 2019-03-12.
+//  Copyright © 2019 se.Benjamin.Aronsson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
